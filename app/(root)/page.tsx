3ce80a1ee44cf1692434826c0075697d9a932de5
Rishabh <rishabh.sharma3@appinventiv.com>
2025-07-09 12:21:48 +0530

    Started with the hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startup, <br /> Connct With Entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competetions.</p>
+      </section>
     </>
   );
 }
