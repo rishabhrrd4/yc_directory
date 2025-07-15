e8b6c8876af67d81ed5bb4ca98022ad1d9dcb129
Rishabh <rishabh.sharma3@appinventiv.com>
2025-07-15 11:27:49 +0530

    Implement Partial Pre-rendering(PPR)

--- lib/queries.ts
+++ lib/queries.ts
@@ -6,3 +6,10 @@ export const STARTUPS_QUERY =
     _id, name, image, bio
   }, views, description, category, image
 }`);
+
+export const STARTUP_BY_ID_QUERY =
+  defineQuery(`*[_type == "startup" && _id == $id][0] {
+  _id, title, slug, _createdAt, author -> {
+    _id, name, username, image, bio
+  }, views, description, category, image, pitch
+}`);
